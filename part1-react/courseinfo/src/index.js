0c1524369731690d98c68879a6a72acf63e9081c
santimenendez <santi_mm11@hotmail.com>
2023-03-03 11:34:08 -0300

    hooks react

--- part1-react/courseinfo/src/index.js
+++ part1-react/courseinfo/src/index.js
@@ -1,54 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { useState } from 'react'
 
-const Title = (props) => {
-  return <h1>{props.course}</h1>
-}
+const rootElement = document.getElementById("root")
 
-const Part = (props) => {
-  return <p>Total = {props.parts.exercise}</p>
-}
+const App = (props) => {
+  const [contadorValue, contadorSuma]= useState(0);
 
-const Content = (props) => {
-  return (
-    <div>
-      <p>{props.parts.name}</p>
-      <p>{props.parts.exercise}</p>
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-  <p>Total = {props.parts.exercise}</p>
-  )
-}
-
-const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [{
-    name: 'Fundamentals of React',
-    exercise: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercise: 7
-  },
-  {
-    name: 'State of a component',
-    exercise: 14
-  }
-]
+  console.log('refresh')
 
   return (
     <div>
-      <Title course={course} />
-      <Content parts={parts[0]} />
-      <Content parts={parts[1]} />
-      <Content parts={parts[2]} />
-      <Total parts={parts[0]}/> 
+      <p>El contador es:</p>
+      <h1>{contadorValue}</h1>
+      <button onClick={() => {
+        contadorSuma(contadorValue + 1)
+      }}>
+        Incrementar
+      </button>
     </div>
   )
 }
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <App/>,
+  rootElement)
